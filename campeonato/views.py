885381b320822d701a753fd2478452b61eed3860
juliobueno <julioignaciob@gmail.com>
2024-10-13 22:03:51 -0300

    agregando los html (falta redirigir los html)

--- campeonato/views.py
+++ campeonato/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Jugador
 
 # Create your views here.
 def  lista_campeonato(request):
     return render(request,'campeonato/lista_campeonato.html',{})
+
